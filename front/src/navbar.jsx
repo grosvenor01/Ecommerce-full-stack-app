5cdbe920372c14f3309a7b848ea9330be41c2068
ouchaou.chafaa <chafaaouchaou@gmail.com>
2024-03-23 13:54:34 +0100

    Built the navbar

--- front/src/navbar.jsx
+++ front/src/navbar.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
-//import the red chakra color
-
-
+import { Box,Center,Image,Stack } from '@chakra-ui/react'
+import Cart from './assets/cart.svg'
+import Points from './assets/points.svg'
+import Racklogo from './assets/Racklogo.svg'
+import Profile from './assets/profile.svg'
+import Heart from './assets/heart.svg'
+import { Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { HStack, VStack } from '@chakra-ui/react'
 const Navbar = () => {
-  return (
-    <Box bg={"reD"}   className='color1'>navbar</Box>
+  return (<>
+    <Box boxSize='lr' mt={"1rem"} >
+
+
+    <HStack  spacing='24px' >
+  <Image className="navlogohover"  src={Racklogo} alt='Rack logo' />
+
+
+  <InputGroup bg={""} width={"75%"} alignItems="center">
+    <InputLeftElement  pointerEvents='none'>
+      <SearchIcon mt={"10px"}  color='white' />
+    </InputLeftElement>
+    <Input height={"3rem"} borderRadius={"20px"} type='text' placeholder='Search product' />
+</InputGroup>
+
+
+
+  <HStack spacing='24px'>
+  <Image className='navimghover' src={Profile} alt='Rack logo' />
+  <Image className='navimghover' src={Heart} alt='Rack logo' />
+  <Image className='navimghover' src={Cart} alt='Rack logo' />
+  <Image className='navimghover' src={Points} alt='Rack logo' />
+</HStack>
+
+  </HStack>
+
+
+
     
+
+
+
+
+
+
+</Box>
+    </>
   )
 }
 
