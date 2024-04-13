cf1f7eed2f91c4c89592780fc04cfa542a6213fd
abdelkader <96534255+grosvenor01@users.noreply.github.com>
2024-04-13 23:05:32 +0100

    Update views.py

--- back/ecom/ecomApp/views.py
+++ back/ecom/ecomApp/views.py
@@ -66,7 +66,7 @@ class WishlistListCreateView(generics.ListCreateAPIView):
     serializer_class = WishlistSerializer1
 
 class WishlistDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Wishlist.objects.all()
+    queryset = wishlist.objects.all()
     serializer_class = WishlistSerializer
     def get(self , request , pk):
         user_id = self.kwargs['pk']
